3891b1d72353126453d11de0b707d6296c2c1b49
Rishikesh Pravin Yelne <rishikeshyelne95@gmail.com>
2024-01-06 15:21:32 -0500

    MySQL in Ansible

--- ansible-1/node-app/app.js
+++ ansible-1/node-app/app.js
@@ -1,12 +1,33 @@
 // Import the Express module
 const express = require('express');
+const mysql = require('mysql2/promise');
 
 // Create an instance of Express
 const app = express();
 
+const dbConfig = {
+  host: 'localhost',
+  user: '',
+  password: '',
+  database: 'sample_db',
+};
+
 // Define a route for the root URL
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+app.get('/data', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const [rows] = await connection.execute('SELECT * FROM sample_table');
+    connection.end();
+
+    res.json(rows);
+  } catch (error) {
+    console.error('Error fetching records:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Start the server on port 3000
